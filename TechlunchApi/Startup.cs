6af8368daeb281a0f62e0f09a3ce21fd0bd55fed
haris.khan <haris.khan@TS-HARIS-PC>
2022-08-15 15:17:13 +0500

    testing

--- TechlunchApi/Startup.cs
+++ TechlunchApi/Startup.cs
@@ -39,7 +39,7 @@ namespace TechlunchApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            //for authorization
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
