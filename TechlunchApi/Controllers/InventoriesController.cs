ea471d88bb7206ea45f7b279ebbe77bb8e3c769a
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-16 17:20:48 +0500

    show inventory get apis

---
+++ TechlunchApi/Controllers/InventoriesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechlunchApi.Data;
+using TechlunchApi.Models;
+
+namespace TechlunchApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoriesController : ControllerBase
+    {
+        private readonly TechlunchDbContext _context;
+
+        public InventoriesController(TechlunchDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Inventories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Inventory>>> GetInventory()
+        {
+            return await _context.Inventory.Include(i => i.IngredientFK).ToListAsync();
+        }
+
+        // GET: api/Inventories/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Inventory>> GetInventory(int id)
+        {
+            var inventory = await _context.Inventory.Include(i => i.IngredientFK)
+                .FirstOrDefaultAsync(i => i.Id == id);
+
+            if (inventory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(inventory);
+        }
+
+    }
+}
