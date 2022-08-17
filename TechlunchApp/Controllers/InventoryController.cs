612ee244c194b43f0164d796488ae3a67c314c38
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-17 19:24:05 +0500

    show inventory frontend added

---
+++ TechlunchApp/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechlunchApp.Common;
+using TechlunchApp.ViewModels;
+
+namespace TechlunchApp.Controllers
+{
+    public class InventoryController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<InventoryViewModel> inventoryList = new List<InventoryViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}generalinventories"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    inventoryList = JsonConvert.DeserializeObject<List<InventoryViewModel>>(apiResponse);
+                }
+            }
+            return View(inventoryList);
+        }
+    }
+}
---
+++ TechlunchApp/ViewModels/InventoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechlunchApp.ViewModels
+{
+    public class InventoryViewModel
+    {
+        public int Id { get; set; }
+
+        public int IngredientId { get; set; }
+
+        [ForeignKey("IngredientId")]
+        public IngredientViewModel IngredientFK { get; set; }
+
+        [Required]
+        public int AvailableQuantity { get; set; }
+
+        [Required]
+        public float AveragePrice { get; set; }
+    }
+}
