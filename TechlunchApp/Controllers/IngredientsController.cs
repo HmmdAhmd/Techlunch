cf3c6a56e2639ee3875f1bc0eeac49239dfa9c32
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-17 13:24:26 +0500

    show ingredients frontend

---
+++ TechlunchApp/Controllers/IngredientsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechlunchApp.ViewModels;
+
+namespace TechlunchApp.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        private string apiUrl;
+
+        public IngredientsController(IConfiguration configuration)
+        {
+            apiUrl = configuration.GetValue<string>("apiUrl");
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<IngredientViewModel> ingredients = new List<IngredientViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(apiUrl+"ingredients"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ingredients = JsonConvert.DeserializeObject<List<IngredientViewModel>>(apiResponse);
+                }
+            }
+            return View(ingredients);
+        }
+
+    }
+}
---
+++ TechlunchApp/ViewModels/IngredientViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TechlunchApp.ViewModels
+{
+    public class IngredientViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
