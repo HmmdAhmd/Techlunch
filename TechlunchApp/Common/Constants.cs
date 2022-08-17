68a3c3015da7a4d3dad0f3c3551c52faf305dea4
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-17 16:36:26 +0500

    constant for apiUrl added

---
+++ TechlunchApp/Common/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace TechlunchApp.Common
+{
+    public static class Constants
+    {
+        public const string ApiUrl = "https://localhost:44357/api/";
+    }
+}
--- TechlunchApp/Controllers/IngredientsController.cs
+++ TechlunchApp/Controllers/IngredientsController.cs
@@ -1,28 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using TechlunchApp.Common;
 using TechlunchApp.ViewModels;
 
 namespace TechlunchApp.Controllers
 {
     public class IngredientsController : Controller
     {
-        private string apiUrl;
-
-        public IngredientsController(IConfiguration configuration)
-        {
-            apiUrl = configuration.GetValue<string>("apiUrl");
-        }
-
         public async Task<IActionResult> Index()
         {
             List<IngredientViewModel> ingredients = new List<IngredientViewModel>();
             using (var httpClient = new HttpClient())
             {
-                using (var response = await httpClient.GetAsync(apiUrl+"ingredients"))
+                using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}ingredients"))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
                     ingredients = JsonConvert.DeserializeObject<List<IngredientViewModel>>(apiResponse);
