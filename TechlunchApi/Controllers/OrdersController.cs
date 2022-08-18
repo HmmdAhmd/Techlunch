2600c3f3dae7fc00e4ced8e4bcaa7fca29edf445
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-18 12:40:53 +0500

    view orders api added

---
+++ TechlunchApi/Controllers/OrdersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TechlunchApi.Data;
+using TechlunchApi.Models;
+
+namespace TechlunchApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly TechlunchDbContext _context;
+
+        public OrdersController(TechlunchDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders.OrderByDescending(o => o.Id).ToListAsync();
+        }
+    }
+}
