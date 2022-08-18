fe55dde052abb7ac193baa5cb7904e70fa0767d7
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-18 11:09:50 +0500

    add inventory frontend

--- TechlunchApp/Controllers/InventoryController.cs
+++ TechlunchApp/Controllers/InventoryController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using TechlunchApp.Common;
 using TechlunchApp.ViewModels;
@@ -12,16 +13,47 @@ namespace TechlunchApp.Controllers
     {
         public async Task<IActionResult> Index()
         {
-            List<InventoryViewModel> inventoryList = new List<InventoryViewModel>();
+            List<GeneralInventoryViewModel> inventoryList = new List<GeneralInventoryViewModel>();
             using (var httpClient = new HttpClient())
             {
                 using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}generalinventories"))
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
-                    inventoryList = JsonConvert.DeserializeObject<List<InventoryViewModel>>(apiResponse);
+                    inventoryList = JsonConvert.DeserializeObject<List<GeneralInventoryViewModel>>(apiResponse);
                 }
             }
             return View(inventoryList);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            List<IngredientViewModel> Ingredients = new List<IngredientViewModel>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var IngredientsResponse = await httpClient.GetAsync($"{Constants.ApiUrl}Ingredients"))
+                {
+                    string IngredientApiResponse = await IngredientsResponse.Content.ReadAsStringAsync();
+                    Ingredients = JsonConvert.DeserializeObject<List<IngredientViewModel>>(IngredientApiResponse);
+
+                }
+            }
+
+            return View(Ingredients);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(InventoryViewModel inventoryObj)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(inventoryObj), Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync($"{Constants.ApiUrl}inventories", content);
+
+            }
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- TechlunchApp/ViewModels/GeneralInventoryViewModel.cs
+++ TechlunchApp/ViewModels/GeneralInventoryViewModel.cs
@@ -1,27 +1,21 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TechlunchApp.ViewModels
 {
-    public class InventoryViewModel
+    public class GeneralInventoryViewModel
     {
         public int Id { get; set; }
 
-        [Required]
         public int IngredientId { get; set; }
 
         [ForeignKey("IngredientId")]
         public IngredientViewModel IngredientFK { get; set; }
 
+        [Required]
+        public int AvailableQuantity { get; set; }
 
         [Required]
-        public int Quantity { get; set; }
-
-        [Required]
-        public float Price { get; set; }
-
-        [Required]
-        public DateTime AddedOn { get; set; }
+        public float AveragePrice { get; set; }
     }
 }
