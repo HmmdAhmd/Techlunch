cfa4e918907c9b56628fba6eff1c86885a38d6d0
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-18 13:03:07 +0500

    some changes made in ingredient history UI

---
+++ TechlunchApp/Controllers/OrdersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechlunchApp.Common;
+using TechlunchApp.ViewModels;
+
+namespace TechlunchApp.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<OrderViewModel> orders = new List<OrderViewModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}orders"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    orders = JsonConvert.DeserializeObject<List<OrderViewModel>>(apiResponse);
+                }
+            }
+            return View(orders);
+        }
+    }
+}
---
+++ TechlunchApp/ViewModels/OrderViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechlunchApp.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public float TotalPrice { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
