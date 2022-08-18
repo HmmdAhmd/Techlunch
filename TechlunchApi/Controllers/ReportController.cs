7beb34646a1d877dc75ab8d9f37cc5f3c816112c
haris.khan <haris.khan@TS-HARIS-PC>
2022-08-18 12:02:32 +0500

    Initial Commit

---
+++ TechlunchApi/Controllers/ReportController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using TechlunchApi.Data;
+using TechlunchApi.Models;
+
+namespace TechlunchApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+
+        private readonly TechlunchDbContext _context;
+
+        public ReportController(TechlunchDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{StartingTime, EndingTime}")]
+        public string GetReport(DateTime StartingTime, DateTime EndingTime)
+        {
+            
+            int TotalOrders = _context.Orders.Where(tbl => tbl.Status == true && tbl.CreatedAt <= EndingTime && tbl.CreatedAt >= StartingTime).Count();
+            float TotalSales = _context.Orders.Where(tbl => tbl.Status == true && tbl.CreatedAt <= EndingTime && tbl.CreatedAt >= StartingTime).Sum(p=>p.TotalPrice);
+            
+            //it depends upon the task 7.1.1 (Cost Estimation Function)
+            float Cost = 0;
+
+            var report = new
+            {
+                TotalOrders = TotalOrders,
+                TotalSales = TotalSales,
+                TotalProfit = TotalSales - Cost
+            };
+            string jsonData = JsonConvert.SerializeObject(report);
+            return jsonData;
+        }
+    }
+}
---
+++ TechlunchApp/Controllers/ReportController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechlunchApp.Common;
+using TechlunchApp.ViewModels;
+
+namespace TechlunchApp.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GenerateReport(DateTime StartingTime, DateTime EndingTime)
+        {
+            string St = StartingTime.ToString("MM/dd/yyyy hh:mm:ss tt");
+            string Et = EndingTime.ToString("MM/dd/yyyy hh:mm:ss tt");
+            ReportViewModel report = new ReportViewModel();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}report/getreport?StartingTime={St}&EndingTime={Et}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    report = JsonConvert.DeserializeObject<ReportViewModel>(apiResponse);
+                }
+            }
+            ViewData["StartingTime"] = StartingTime;
+            ViewData["EndingTime"] = EndingTime;
+            return View("ShowReport",report);
+        }
+
+    }
+}
