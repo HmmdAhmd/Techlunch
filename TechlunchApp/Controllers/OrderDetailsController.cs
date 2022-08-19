a4fe6c46fd40121ecf0f3c77169f2da1621fd5c1
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-19 10:54:21 +0500

    delete order item frontend

---
+++ TechlunchApp/Controllers/OrderDetailsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TechlunchApp.Common;
+
+namespace TechlunchApp.Controllers
+{
+    public class OrderDetailsController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (var response = await httpClient.DeleteAsync($"{Constants.ApiUrl}orderdetails/{id}"))
+            //    {
+            //        await response.Content.ReadAsStringAsync();
+            //    }
+            //}
+            return RedirectToAction("AddItems", "Orders", new {id=1 });
+        }
+    }
+}
