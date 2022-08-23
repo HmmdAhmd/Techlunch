dde383db94bccc6db4be19ccbb4016b17e34793f
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-23 19:04:17 +0500

    add order new

--- TechlunchApp/Controllers/OrdersController.cs
+++ TechlunchApp/Controllers/OrdersController.cs
@@ -29,11 +29,11 @@ namespace TechlunchApp.Controllers
             return View(orders);
         }
 
-        [HttpGet]
-        public IActionResult Create()
-        {
-            return View();
-        }
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
 
         [HttpPost]
         public async Task<IActionResult> Confirm(int id)
@@ -92,15 +92,33 @@ namespace TechlunchApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(OrderViewModel orderObj)
+        public async Task<IActionResult> Create()
         {
+            OrderViewModel orderObj = new OrderViewModel
+            {
+                TotalPrice = 0,
+                Status = true,
+                Confirmed = false,
+                CreatedAt = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-ddTHH:mm"))
+            };
             using (var httpClient = new HttpClient())
             {
                 StringContent content = new StringContent(JsonConvert.SerializeObject(orderObj), Encoding.UTF8, "application/json");
                 var response = await httpClient.PostAsync($"{Constants.ApiUrl}orders", content);
+
+                using (var Response = await httpClient.GetAsync($"{Constants.ApiUrl}orders/latest"))
+                {
+                    if (!Response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("Index");
+                    }
+
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    orderObj = JsonConvert.DeserializeObject<OrderViewModel>(apiResponse);
+                }
             }
 
-            return RedirectToAction("Index");
+            return RedirectToAction("AddItems", "Orders", new { id = orderObj.Id });
         }
 
         [HttpGet]
@@ -133,11 +151,36 @@ namespace TechlunchApp.Controllers
                     foodItems = JsonConvert.DeserializeObject<List<FoodItemViewModel>>(apiResponse);
                 }
 
-                using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}orderdetails/order/{id}"))
+                for(int i=0;i<foodItems.Count;)
                 {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    orderDetailsList = JsonConvert.DeserializeObject<List<OrderDetailsViewModel>>(apiResponse);
+                    List<FoodItemIngredientViewModel> temp = new List<FoodItemIngredientViewModel>();
+                    using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}fooditemingredients/{foodItems[i].Id}"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        temp = JsonConvert.DeserializeObject<List<FoodItemIngredientViewModel>>(apiResponse);
+                    }
+                    if (temp.Count == 0)
+                    {
+                        foodItems.Remove(foodItems[i]);
+                    } else
+                    {
+                        orderDetailsList.Add(new OrderDetailsViewModel
+                        {
+                            OrderId = id,
+                            FoodItemId = foodItems[i].Id,
+                            FoodItemFK = foodItems[i],
+                            Quantity = 0,
+                            Price = 0
+                        });
+                        i++;
+                    }
                 }
+
+                //using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}orderdetails/order/{id}"))
+                //{
+                //    string apiResponse = await response.Content.ReadAsStringAsync();
+                //    orderDetailsList = JsonConvert.DeserializeObject<List<OrderDetailsViewModel>>(apiResponse);
+                //}
 
             }
 
@@ -152,45 +195,54 @@ namespace TechlunchApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddItems(OrderDetailsViewModel orderDetail)
+        public async Task<IActionResult> AddItems(List<OrderDetailsViewModel> orderDetails)
         {
-            float estPrice = await CheckIngredientsAvailability(orderDetail.FoodItemId, orderDetail.Quantity);
-
-            FoodItemViewModel foodItem = new FoodItemViewModel();
-
-            using (var httpClient = new HttpClient())
-            {
-                using (var Response = await httpClient.GetAsync($"{Constants.ApiUrl}fooditems/{orderDetail.FoodItemId}"))
-                {
-                    string apiResponse = await Response.Content.ReadAsStringAsync();
-                    foodItem = JsonConvert.DeserializeObject<FoodItemViewModel>(apiResponse);
-                }
-            }
-
-            if (estPrice != 0)
-            {
-                float price = 0;
-
-                using (var httpClient = new HttpClient())
-                {
-                    orderDetail.Price = foodItem.Price * orderDetail.Quantity;
-                    orderDetail.EstimatedPrice = estPrice;
-                    price = orderDetail.Price;
-
-                    StringContent content = new StringContent(JsonConvert.SerializeObject(orderDetail), Encoding.UTF8, "application/json"); ;
-                    var response = await httpClient.PostAsync($"{Constants.ApiUrl}orderdetails", content);
-                }
-
-                await IncrementOrderPrice(orderDetail.OrderId, price);
 
 
-            } else
-            {
-                message = $"Food Item: {foodItem.Name} cannot be added as specified quantity is unavailable";
-            }
-
-            return RedirectToAction("AddItems", "Orders", new { id = orderDetail.OrderId });
+            return RedirectToAction("Index");
         }
+
+        //[HttpPost]
+        //public async Task<IActionResult> AddItems(OrderDetailsViewModel orderDetail)
+        //{
+        //    float estPrice = await CheckIngredientsAvailability(orderDetail.FoodItemId, orderDetail.Quantity);
+
+        //    FoodItemViewModel foodItem = new FoodItemViewModel();
+
+        //    using (var httpClient = new HttpClient())
+        //    {
+        //        using (var Response = await httpClient.GetAsync($"{Constants.ApiUrl}fooditems/{orderDetail.FoodItemId}"))
+        //        {
+        //            string apiResponse = await Response.Content.ReadAsStringAsync();
+        //            foodItem = JsonConvert.DeserializeObject<FoodItemViewModel>(apiResponse);
+        //        }
+        //    }
+
+        //    if (estPrice != 0)
+        //    {
+        //        float price = 0;
+
+        //        using (var httpClient = new HttpClient())
+        //        {
+        //            orderDetail.Price = foodItem.Price * orderDetail.Quantity;
+        //            orderDetail.EstimatedPrice = estPrice;
+        //            price = orderDetail.Price;
+
+        //            StringContent content = new StringContent(JsonConvert.SerializeObject(orderDetail), Encoding.UTF8, "application/json"); ;
+        //            var response = await httpClient.PostAsync($"{Constants.ApiUrl}orderdetails", content);
+        //        }
+
+        //        await IncrementOrderPrice(orderDetail.OrderId, price);
+
+
+        //    }
+        //    else
+        //    {
+        //        message = $"Food Item: {foodItem.Name} cannot be added as specified quantity is unavailable";
+        //    }
+
+        //    return RedirectToAction("AddItems", "Orders", new { id = orderDetail.OrderId });
+        //}
 
         public async Task<IActionResult> Delete(int id)
         {
