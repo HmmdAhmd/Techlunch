e0badce1d4d3244ab0ab386f09b468174e58b0f7
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-24 00:32:05 +0500

    reports validation added

--- TechlunchApp/Controllers/ReportController.cs
+++ TechlunchApp/Controllers/ReportController.cs
@@ -11,28 +11,49 @@ namespace TechlunchApp.Controllers
 {
     public class ReportController : Controller
     {
+
+        private static bool message = false;
         public IActionResult Index()
         {
+            ViewBag.Message = message;
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> GenerateReport(DateTime StartingTime, DateTime EndingTime)
+        public async Task<IActionResult> GenerateReport(GenerateReportViewModel generateReportObj)
         {
-            string St = StartingTime.ToString("MM/dd/yyyy hh:mm:ss tt");
-            string Et = EndingTime.ToString("MM/dd/yyyy hh:mm:ss tt");
-            ReportViewModel report = new ReportViewModel();
-            using (var httpClient = new HttpClient())
+            message = false;
+
+            if (ModelState.IsValid)
             {
-                using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}report/getreport?StartingTime={St}&EndingTime={Et}"))
+                DateTime st = (DateTime)generateReportObj.StartingTime;
+                DateTime et = (DateTime)generateReportObj.EndingTime;
+
+                if (st > et)
                 {
-                    string apiResponse = await response.Content.ReadAsStringAsync();
-                    report = JsonConvert.DeserializeObject<ReportViewModel>(apiResponse);
+                    ViewBag.Message = true;
+                    return View("Index", generateReportObj);
                 }
+
+                
+                string St = st.ToString("MM/dd/yyyy hh:mm tt");
+                string Et = et.ToString("MM/dd/yyyy hh:mm tt");
+                ReportViewModel report = new ReportViewModel();
+                using (var httpClient = new HttpClient())
+                {
+                    using (var response = await httpClient.GetAsync($"{Constants.ApiUrl}report/getreport?StartingTime={St}&EndingTime={Et}"))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        report = JsonConvert.DeserializeObject<ReportViewModel>(apiResponse);
+                    }
+                }
+                ViewData["StartingTime"] = generateReportObj.StartingTime;
+                ViewData["EndingTime"] = generateReportObj.EndingTime;
+                return View("ShowReport", report);
             }
-            ViewData["StartingTime"] = StartingTime;
-            ViewData["EndingTime"] = EndingTime;
-            return View("ShowReport",report);
+
+            ViewBag.Message = message;
+            return View("Index", generateReportObj);
         }
 
     }
---
+++ TechlunchApp/ViewModels/GenerateReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechlunchApp.ViewModels
+{
+    public class GenerateReportViewModel
+    {
+        [Required(ErrorMessage = "Please enter start date")]
+        public DateTime? StartingTime { get; set; }
+
+        [Required(ErrorMessage = "Please enter end date")]
+        public DateTime? EndingTime { get; set; }
+    }
+}
