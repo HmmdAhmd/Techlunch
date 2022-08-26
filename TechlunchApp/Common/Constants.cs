5f0e2a8cb1dc3849339bbbbafe23755ba94a1496
haris.khan <haris.khan@TS-HARIS-PC>
2022-08-26 19:32:56 +0500

    commit1

--- TechlunchApp/Common/Constants.cs
+++ TechlunchApp/Common/Constants.cs
@@ -2,7 +2,10 @@
 {
     public static class Constants
     {
-        public const string ApiUrl = "https://localhost:5001/api/";
-
+        public const string DateFormat = "MM/dd/yyyy";
+        public const string LoginErrorMessage = "The Username or password is incorrect";
+        public const string UserExistErrorMessage = "User Already Exist";
+        public const string PasswordErrorMessage = "Password must contain Non-Alphanumeric,Digit and Upper Case letter";
+        public const string SignupSuccessMessage = "Account Created Successfully";
     }
 }
