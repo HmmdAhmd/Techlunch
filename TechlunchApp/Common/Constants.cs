d5d76099b9a0416cf729067b447afc82c9ade73a
hmmdAhmd <hammad.ahmed@techno-soft.com>
2022-08-26 14:33:52 +0500

    changtes

--- TechlunchApp/Common/Constants.cs
+++ TechlunchApp/Common/Constants.cs
@@ -3,6 +3,7 @@
     public static class Constants
     {
         public const string ApiUrl = "https://localhost:44357/api/";
+        //public const string ApiUrl = "http://10.10.10.189:5056/api/";
 
     }
 }
